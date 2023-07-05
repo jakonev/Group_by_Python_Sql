4895ae83e3d81747cc9b762a6f360c0aa0dee1ac
jakonev <136200070+jakonev@users.noreply.github.com>
2023-07-05 18:45:42 -0300

    group_by_sql_pyhton
    sql in python

---
+++ classifica_group_by.py
@@ -0,0 +1,36 @@
+from itertools import groupby
+
+
+alunos = [
+    {'nome': 'Luiz', 'nota': 'A'},
+    {'nome': 'Letícia', 'nota': 'B'},
+    {'nome': 'Fabrício', 'nota': 'A'},
+    {'nome': 'Rosemary', 'nota': 'C'},
+    {'nome': 'Joana', 'nota': 'D'},
+    {'nome': 'João', 'nota': 'A'},
+    {'nome': 'Eduardo', 'nota': 'B'},
+    {'nome': 'André', 'nota': 'A'},
+    {'nome': 'Anderson', 'nota': 'C'},
+]
+# alunos = ['a', 'b', 'c']
+# grupos = groupby(alunos)
+#
+# for chave, grupo, in grupos:
+#     print(chave)
+#     print(list(grupo))
+def ordena(aluno):
+    return aluno['nota']
+
+
+
+# ou com a funcao ordena para manter o key lambda com ordena
+# alunos_agrupados = sorted(alunos, key=lambda a: a['nota'])
+# grupos = groupby(alunos_agrupados, key=lambda a: a['nota'])
+
+alunos_agrupados = sorted(alunos, key=ordena)
+grupos = groupby(alunos_agrupados, key=ordena)
+
+
+for chave, grupo in grupos:
+    print(chave)
+    print(list(grupo))
